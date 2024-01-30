e02a851b07b2cc5ccc723904ffc7dfe4592113ef
Gokul <gokulsrinivask2020@gmail.com>
2024-01-30 20:09:51 +0530

    29th jan

---
+++ implement_queue_using_stacks.cpp
@@ -0,0 +1,32 @@
+class MyQueue {
+public:
+stack<int> input,output;
+    MyQueue() {
+        
+
+    }
+    
+    void push(int x) {
+        input.push(x);
+    }
+    
+    int pop() {
+        int topelement = peek();
+        output.pop();
+        return topelement;
+    }
+    
+    int peek() {
+        if(output.empty()){
+            while(!input.empty()){
+                output.push(input.top());
+                input.pop();
+            }
+        }
+        return output.top();
+    }
+    
+    bool empty() {
+        return (input.empty() && output.empty());
+    }
+};

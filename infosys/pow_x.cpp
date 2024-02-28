638824c3837f347ac39b42c92e6a5b9248e475c0
Gokul <gokulsrinivask2020@gmail.com>
2024-02-28 10:16:07 +0530

    feb28th

--- infosys/pow_x.cpp
+++ infosys/pow_x.cpp
@@ -22,3 +22,26 @@ public:
         return ans;
     }
 };
+// class Solution {
+// public:
+//     double myPow(double x, int n) {
+//         long long newn = n;
+//         double ans = 1;
+//         if(newn<0){
+//             newn = (-1)*newn;
+//         }
+//         while(newn>0){
+//             if(newn%2==0){
+//                 x=x*x;
+//                 newn/=2;
+//             }else{
+//                 ans=ans*x;
+//                 newn-=1;
+//             }
+//         }
+//         if(n<0){
+//             ans=(double)(1)/(double)(ans);
+//         }
+//         return ans;
+//     }
+// };
